0c661bf1105375e4e1250f8c9d7b7da57e314873
Ben Tan <bst15@graphic25.doc.ic.ac.uk>
2016-11-22 12:55:02 +0000

    lock is removed from platformaccess

--- src/PlatformAccess.java
+++ src/PlatformAccess.java
@@ -1,27 +1,17 @@
 public class PlatformAccess {
 
-    //Lock object
-    private final Object platformLock = new Object();
-
     // Flag determining whether there is a car waiting on the platform
     protected boolean platformOccupied = false;
 
-    public void arrive() throws InterruptedException {
-        if (platformOccupied) {
-            synchronized (platformLock) {
-            // wait if there's a car on platform
-            platformLock.wait();
-            }
-        }
+    public synchronized void arrive() throws InterruptedException {
+        while (platformOccupied) wait();
         platformOccupied = true; // signify that there is a car on platform
     }
 
     public synchronized void depart() {
         if (platformOccupied) { // cannot depart unless there's a car on platform
             platformOccupied = false; // car had left and platform is accessible
-            synchronized (platformLock) {
-                platformLock.notifyAll(); // notify other that a car had departed
-            }
+            notifyAll(); // notify other that a car had departed
         }
     }
 
